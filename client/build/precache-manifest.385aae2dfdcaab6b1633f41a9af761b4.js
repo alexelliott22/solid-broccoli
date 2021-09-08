25fe009a0d2be093de364222b0aceaeb60522a77
alexelliott22 <81639053+alexelliott22@users.noreply.github.com>
2021-09-08 17:38:51 -0400

    deploy test

---
+++ client/build/precache-manifest.385aae2dfdcaab6b1633f41a9af761b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b483c129a4cdb19f1656c6cfd96d3414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a98c728553a8ca0047a5",
+    "url": "/static/css/2.fa6c921b.chunk.css"
+  },
+  {
+    "revision": "67bfb04236f897fb4a04",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "a98c728553a8ca0047a5",
+    "url": "/static/js/2.d37ed6a6.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/static/js/2.d37ed6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67bfb04236f897fb4a04",
+    "url": "/static/js/main.d4c96e38.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
